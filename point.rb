ce3eed43b51051a163674733bfd2cb219b9bd0e7
Tyler Lemburg <trlemburg@gmail.com>
2023-11-08 20:55:15 -0600

    Add the handy Point class

---
+++ point.rb
@@ -0,0 +1,40 @@
+class Point
+  attr_writer :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def px
+    self.new(x+1, y)
+  end
+
+  def py
+    self.new(x, y+1)
+  end
+
+  def mx
+
+  end
+
+  def my
+
+  end
+
+  def to_s
+    "<#{x},#{y}>"
+  end
+
+  def eql?(other)
+    x == other.x && y == other.y
+  end
+
+  def hash
+    to_s.hash
+  end
+
+  def ==(other)
+    x == other.x && y == other.y
+  end
+end
