8b9b43ce06417de9ef99ea8f8aa28ba29803612f
Tyler Lemburg <trlemburg@gmail.com>
2023-12-03 08:09:36 -0600

    Fix readme

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Advent of Code
-
-Advent of Code runs December 1-25 because thats what people understand Advent to be.
--- part1.rb
+++ part1.rb
@@ -7,7 +7,11 @@ test = ARGV[0] == '--test'
 in_file = test ? 'test_in.txt' : 'real_in.txt'
 
 lines = []
+map = {}
  
-File.readlines(in_file, chomp: true).each do |line|
+File.readlines(in_file, chomp: true).each_with_index do |line, y|
   lines << line
+  line.chars.each_with_index do |char, x|
+    map[Point.new(x,y)] = char
+  end
 end
