a1d380ba3799b09030463970235c1d63b2a3ee0b
Tyler Lemburg <trlemburg@gmail.com>
2023-12-25 21:22:44 -0600

    Start 2023 25

---
+++ 2023/25/part1.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require_relative '../../point'
+require 'set'
+
+test = ARGV[0] == '--test'
+
+in_file = test ? 'test_in.txt' : 'real_in.txt'
+
+lines = []
+map = {}
+ 
+File.readlines(in_file, chomp: true).each_with_index do |line, y|
+  lines << line
+  line.chars.each_with_index do |char, x|
+    map[Point.new(x,y)] = char
+  end
+end
+
+x_max = lines[0].length - 1
+y_max = lines.count - 1
+
