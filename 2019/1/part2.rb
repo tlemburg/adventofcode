4484f4a3c50dd1795a874f81a5987baa797f9876
Tyler Lemburg <trlemburg@gmail.com>
2024-11-28 20:39:17 -0600

    2019 #1

---
+++ 2019/1/part2.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require_relative '../../point'
+require 'set'
+
+test = ARGV[0] == '--test'
+
+in_file = test ? 'test_in.txt' : 'real_in.txt'
+
+lines = []
+map = {}
+ 
+File.readlines(in_file, chomp: true).each_with_index do |line, y|
+  lines << line
+  line.chars.each_with_index do |char, x|
+    map[Point.new(x,y)] = char
+  end
+end
+
+x_max = lines[0].length - 1
+y_max = lines.count - 1
+
+sum = 0
+lines.each do |line|
+  mass = line.to_i
+  fuel = mass / 3 - 2
+
+  while fuel > 0
+    sum += fuel
+    fuel = fuel / 3 - 2
+  end
+end
+
+puts sum
