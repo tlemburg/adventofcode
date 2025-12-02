be73725613efc0f0b6ca854afe616a436b8417ae
tlemburg <tyler.lemburg@ideonapi.com>
2025-12-02 10:53:10 -0600

    Improvements

--- 2025/day1/target/debug/day1.d
+++ 2025/day1/target/debug/day1.d
@@ -1,1 +1,1 @@
-/home/tyler/dev/adventofcode/2025/day1/target/debug/day1: /home/tyler/dev/adventofcode/2025/day1/src/main.rs
+/Users/tyler.lemburg/dev/adventofcode/2025/day1/target/debug/day1: /Users/tyler.lemburg/dev/adventofcode/2025/day1/src/main.rs
