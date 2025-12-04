353b723b917c536cc481076dfbb0467413adcaf8
Tyler Lemburg <trlemburg@gmail.com>
2025-12-04 10:22:40 -0600

    2025 #4

--- 2025/day4/src/main.rs
+++ 2025/day4/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
@@ -20,6 +21,7 @@ fn main() {
     }
 }
 
+#[derive(Eq, Hash, PartialEq, Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -27,18 +29,93 @@ struct Point {
 
 fn part1(lines: &Vec<&str>) {
     let mut total = 0;
-    let min_x = 0;
-    let min_y = 0;
-    let max_x = lines[0].len() - 1;
-    let max_y = lines.count() - 1;
+    let min_x: i32 = 0;
+    let min_y: i32 = 0;
+    let max_x: i32 = (lines[0].len() - 1) as i32;
+    let max_y: i32 = (lines.len() - 1) as i32;
+
+    let mut rolls = HashSet::<Point>::new();
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, character) in line.chars().enumerate() {
+            if character == '@' {
+                rolls.insert(Point {
+                    x: x as i32,
+                    y: y as i32,
+                });
+            }
+        }
+    }
+
+    for x in min_x..=max_x {
+        for y in min_y..=max_y {
+            if rolls.contains(&Point { x, y }) {
+                let mut adjacent = 0;
+                for i in (x - 1)..=(x + 1) {
+                    for j in (y - 1)..=(y + 1) {
+                        if rolls.contains(&Point { x: i, y: j }) {
+                            adjacent += 1;
+                        }
+                    }
+                }
+                if adjacent <= 4 {
+                    total += 1;
+                }
+            }
+        }
+    }
 
     println!("Total: {}", total);
 }
 
 fn part2(lines: &Vec<&str>) {
     let mut total = 0;
+    let min_x: i32 = 0;
+    let min_y: i32 = 0;
+    let max_x: i32 = (lines[0].len() - 1) as i32;
+    let max_y: i32 = (lines.len() - 1) as i32;
 
-    total += 1;
+    let mut rolls = HashSet::<Point>::new();
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, character) in line.chars().enumerate() {
+            if character == '@' {
+                rolls.insert(Point {
+                    x: x as i32,
+                    y: y as i32,
+                });
+            }
+        }
+    }
+
+    loop {
+        let mut removed = HashSet::<Point>::new();
+        for x in min_x..=max_x {
+            for y in min_y..=max_y {
+                if rolls.contains(&Point { x, y }) {
+                    let mut adjacent = 0;
+                    for i in (x - 1)..=(x + 1) {
+                        for j in (y - 1)..=(y + 1) {
+                            if rolls.contains(&Point { x: i, y: j }) {
+                                adjacent += 1;
+                            }
+                        }
+                    }
+                    if adjacent <= 4 {
+                        total += 1;
+                        removed.insert(Point { x, y });
+                    }
+                }
+            }
+        }
+        if removed.len() == 0 {
+            break;
+        } else {
+            for point in removed {
+                rolls.remove(&point);
+            }
+        }
+    }
 
     println!("Total: {}", total);
 }
