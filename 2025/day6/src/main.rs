350d56a0ca54097269d056ce9ea0b7499be27101
Tyler Lemburg <trlemburg@gmail.com>
2025-12-13 11:56:51 -0600

    WIP 2025 #6

---
+++ 2025/day6/src/main.rs
@@ -0,0 +1,58 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = if args.contains(&"--test".to_string()) {
+        "test.txt"
+    } else {
+        "input.txt"
+    };
+
+    let contents = fs::read_to_string(filename).expect("Should have been able to read the file");
+    let lines: Vec<&str> = contents.split('\n').collect();
+
+    if args.contains(&"--part2".to_string()) {
+        part2(&lines);
+    } else {
+        part1(&lines);
+    }
+}
+
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone)]
+struct Range {
+    min: i128,
+    max: i128,
+}
+
+fn part1(lines: &Vec<&str>) {
+    let mut total = 0;
+
+    let mut numbers: Vec<Vec<i32>> = Vec::new();
+
+    let line_count = lines.len();
+    for i in 0..line_count - 1 {
+        let mut row: Vec<i32> = Vec::new();
+        for num in lines[i].split(' ') {
+            let parsed = num.parse().expect("IT DIDNT PARSE");
+            row.push(parsed);
+        }
+        numbers.push(row);
+    }
+
+    println!("Total: {}", total);
+}
+
+fn part2(lines: &Vec<&str>) {
+    let mut total = 0;
+
+    println!("Total: {}", total);
+}
